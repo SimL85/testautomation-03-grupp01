b56cfd26c8d6e3b0b9aa3ab17921e0dbb92bd870
Simone L <lentini.simone@gmail.com>
2021-05-05 11:05:00 +0200

    removed pages

--- front-end-tests/cypress/pages/editBillsPage.js
+++ front-end-tests/cypress/pages/editBillsPage.js
@@ -1,16 +1,16 @@
 /// <reference types="cypress" />
 
-const VALEU_SEK = 'input'
+const VALEU_SEK = ':nth-child(3) > input'
 const PAID_CHECK_B = '.checkbox'
 const SAVE_BTN = '.blue'
 
-function createBill(value, content) {
-    cy.get(VALEU_SEK).type(value)
+function editLastBill(value) {
+    cy.get(VALEU_SEK).clear().type(value)
     cy.get(PAID_CHECK_B).click()
     cy.get(SAVE_BTN).click()
-    cy.contains(content)
+    
 }
 
 exports.default = {
-    createBill
+    editLastBill
 }
