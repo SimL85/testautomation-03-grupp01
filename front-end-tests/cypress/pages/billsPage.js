c2bf9679e669e7ff7f63dd500c555e7e139ce170
ActionArmadillo <62772766+ActionArmadillo@users.noreply.github.com>
2021-05-05 11:10:29 +0200

    edited bills/newbill/clients/newclient pages

--- front-end-tests/cypress/pages/billsPage.js
+++ front-end-tests/cypress/pages/billsPage.js
@@ -3,10 +3,10 @@
 //////////////
 // elements //
 //////////////
-const VIEW_BILLS_PAGE_BUTTON = ':nth-child(3) > .btn'
 const CREATE_BILL_BUTTON = 'h2 > .btn'
 const BILLS_LIST = '.bills'
 const MEATBALLS_BUTTON = '.action'
+const EDIT_BTN = '.menu > :nth-child(1)'
 const DELETE_BUTTON = '.menu > :nth-child(2)'
 
 
@@ -31,13 +31,24 @@ function removeLastBill(){
     cy.get(DELETE_BUTTON).click()
 }
 
+function openEditLastBill() {
+    cy.get(MEATBALLS_BUTTON).last().click()
+    cy.get(EDIT_BTN).click()
+}
+
+function validateEditedBill(content){
+    cy.get(BILLS_LIST).last().should('contain', content)
+}
+
 //////////////////////
 // export functions //
 //////////////////////
 module.exports = {
     openNewBillPage,
+    openEditLastBill,
     validateUnpaidBill,
     validatePaidBill,
+    validateEditedBill,
     removeLastBill
 }
  
--- front-end-tests/cypress/pages/clientsPage.js
+++ front-end-tests/cypress/pages/clientsPage.js
@@ -6,6 +6,7 @@
 
 const CREATE_CLIENT_BUTTON = 'h2 > .btn'
 const MEATBALLS_BUTTON = '.action > img'
+const EDIT_BTN = '.menu > :nth-child(1)'
 const DELETE_BUTTON = '.menu > :nth-child(2)'
 const CLIENT_LIST = '.client'
 
@@ -17,8 +18,19 @@ function openNewClientPage(){
     cy.get(CREATE_CLIENT_BUTTON).click()
 }
 
+function openEditLastClient(){
+    cy.get(MEATBALLS_BUTTON).last().click()
+    cy.get(EDIT_BTN).click()
+}
+
 function validateCreatedClient(name, email, telephone){
     cy.get(CLIENT_LIST).should('contain', name).and('contain', email).and('contain', telephone)
+}
+
+function validateEditLastClient(content) {
+    cy.get(THREE_DOTS_BTN).last().click()
+    cy.get(EDIT_BTN).click()
+    cy.contains(content)
 }
 
 function removeLastClient(){
@@ -31,6 +43,8 @@ function removeLastClient(){
 /////////////////////
 module.exports = {
     openNewClientPage,
+    openEditLastClient,
     validateCreatedClient,
+    validateEditLastClient,
     removeLastClient
 }
