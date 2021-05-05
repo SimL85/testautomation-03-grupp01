9bf4e922bc618fa0aa87f6ba5a444d14c48985ea
Simone L <lentini.simone@gmail.com>
2021-05-05 13:23:50 +0200

    EditClientsPage updated

--- front-end-tests/cypress/pages/EditClientsPage.js
+++ front-end-tests/cypress/pages/EditClientsPage.js
@@ -1,17 +1,21 @@
 /// <reference types="cypress" />
 
-const EMAIL_FIELD = ':nth-child(4) > input'
+const EDIT_NAME_FIELD = ":nth-child(3) > input"
+const EDIT_EMAIL_FIELD = ":nth-child(4) > input"
+const EDIT_PHONE_FIELD = ":nth-child(5) > input"
 const SAVE_BTN = '.blue'
 
-function editClient(email, content) {
-    cy.get(EMAIL_FIELD).clear().type(email)
+function editClient(name, mail, number) {
+    cy.get(EDIT_NAME_FIELD).clear().type(name)
+    cy.get(EDIT_PHONE_FIELD).clear().type(number)
+    cy.get(EDIT_EMAIL_FIELD).clear().type(mail)
     cy.get(SAVE_BTN).click()
-    cy.contains(content)
+
 }
 
 
 
 exports.default = {
-    editClient,
+    editClient
 
 }
